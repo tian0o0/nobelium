ea385ee7f96be45c78be918e495d59ebb231b916
xiet <xiet@133.cn>
2022-07-27 18:37:07 +0800

    👉🏻

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -9,7 +9,7 @@ const BlogPost = ({ post }) => {
         <article key={post.id} className="mb-6 md:mb-8">
           <header className="flex flex-col justify-between md:flex-row md:items-baseline">
             <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
-              {post.title}
+              {'👉🏻 ' + post.title}
             </h2>
             <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
               {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
