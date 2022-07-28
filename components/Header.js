2a6b175363e8aa67d7537378eb47dbb59bc2a384
xiet <xiet@133.cn>
2022-07-28 23:25:19 +0800

    feat: ui

--- components/Header.js
+++ components/Header.js
@@ -22,7 +22,7 @@ const NavBar = () => {
                 className="block ml-4 text-black dark:text-gray-50 nav"
               >
                 <Link href={link.to}>
-                  <a className='nes-btn'>{link.name}</a>
+                  <a className='nes-text'>{link.name}</a>
                 </Link>
               </li>
             )
